5c0ced075fae26f585a54a6203e3d0790c50e907
hyeyomi <127003047+hyeyomi@users.noreply.github.com>
2023-10-18 23:20:00 +0900

    commit

---
+++ js/state.js
@@ -0,0 +1,54 @@
+const stateBtn = document.querySelector('#state-button');
+const stateContainer = document.querySelector('#state-container');
+const stateMsg = document.querySelector('#state-message'); //form
+const stateTitle = document.querySelector('#state_title');
+const stateInputBtn = document.querySelector('#state-message__input__btn');
+const x = document.querySelector('#x');
+let isClick = false; // 버튼 클릭 X가 디폴트 상태
+
+function clickStateBtn() {
+  if (!isClick) {
+    //클릭이 되면 hidden을 지워서 보이도록
+    stateContainer.classList.remove('hidden');
+    isClick = true;
+    // stateMsg.style.position = "relative";
+    // stateTitle.style.position = "relative";
+  } else {
+    stateContainer.classList.add('hidden');
+    isClick = false;
+  }
+}
+
+function saveState(event) {
+  const stateInput = document.querySelector('#state-message__input');
+  event.preventDefault();
+  const state = stateInput.value; // 상태메세지
+  stateInput.value = ''; //새로고침
+  text = state;
+  paintState();
+  saveStateText();
+}
+
+let text = '';
+
+function saveStateText() {
+  localStorage.setItem('text', JSON.stringify(text));
+}
+
+function paintState(txt) {
+  const stateText = document.querySelector('#state_text');
+  stateText.innerText = text;
+  // console.log(text);
+}
+
+const savedTxt = localStorage.getItem('text');
+
+if (savedTxt !== null) {
+  const parsedTxt = JSON.parse(savedTxt);
+  text = parsedTxt;
+  paintState(parsedTxt);
+}
+
+stateBtn.addEventListener('click', clickStateBtn);
+x.addEventListener('click', clickStateBtn);
+stateMsg.addEventListener('submit', saveState);
